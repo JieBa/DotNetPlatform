91c8bcbb4454914e56038005e1ec45be857093b0
Chao <ringeight@outlook.com>
2016-08-09 00:02:31 +0800

    使用内部依赖项存储数据

--- Caching/Caching/CitiesControl.ascx.cs
+++ Caching/Caching/CitiesControl.ascx.cs
@@ -9,41 +9,38 @@ using System.Web.UI.WebControls;
 
 namespace Caching
 {
-    public class CityListInfo
-    {
-        public string Timestamp { get; set; }
-        public string Html { get; set; }
-    }
+
     public partial class CitiesControl : System.Web.UI.UserControl
     {
         private static readonly string _fileName = "/CitiesList.html";
-        private static readonly string _cache_key = "cities_html";
-        private CityListInfo cityInfo;
+        private static readonly string _time_cache_key = "cities_time";
+        private static readonly string _html_cache_key = "cities_html";
         private bool cached = false;
-        protected void Page_Load(object sender, EventArgs e)
+        
+        public string GetCities()
         {
-            cityInfo=Cache[_cache_key] as CityListInfo;
-            if (cityInfo == null)
+            string html = (string) Cache[_html_cache_key];
+            if (html == null)
             {
-                cityInfo=new CityListInfo() {Timestamp = DateTime.Now.ToLongTimeString(),Html = File.ReadAllText(MapPath(_fileName))};
-                Cache.Insert(_cache_key,cityInfo,new CacheDependency(MapPath(_fileName)));
+                html = File.ReadAllText(MapPath(_fileName));
+                Cache.Insert(_html_cache_key,html,new CacheDependency(MapPath(_fileName)));
             }
             else
             {
                 cached = true;
             }
-        }
-
-        public string GetCities()
-        {
-            //return File.ReadAllText(MapPath(_fileName));
-            return cityInfo.Html;
+            return html;
         }
 
         protected string GetTimeStamp()
         {
-            //return DateTime.Now.ToLongTimeString();
-            return cityInfo.Timestamp + (cached ? "<b>Cached</b>" : "");
+            string timeStamp = (string) Cache[_time_cache_key];
+            if (timeStamp == null)
+            {
+                timeStamp = DateTime.Now.ToShortTimeString();
+                Cache.Insert(_time_cache_key,timeStamp,new CacheDependency(null,new[] {_html_cache_key}));
+            }
+            return timeStamp + (cached ? "<b>Cached</b>" : "");
         }
     }
 }
